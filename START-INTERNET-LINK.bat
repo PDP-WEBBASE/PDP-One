5c77b54eb6ae1fc758e47aebf55034c213c3c341
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-07-18 01:13:46 +0330

    Run the temporary internet tunnel with Docker permissions

--- START-INTERNET-LINK.bat
+++ START-INTERNET-LINK.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
+
+if /I not "%~1"=="--elevated" (
+    echo Requesting administrator access for Docker and the internet tunnel...
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~f0' -ArgumentList '--elevated' -Verb RunAs"
+    exit /b
+)
+
 powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\windows\Start-PDPOneTunnel.ps1"
 pause
