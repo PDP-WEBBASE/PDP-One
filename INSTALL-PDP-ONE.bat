2303360b2f80ab5a1920c20e8254c8e8de181e44
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-07-17 22:55:07 +0330

    Make Windows installer request administrator access automatically

--- INSTALL-PDP-ONE.bat
+++ INSTALL-PDP-ONE.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
+
+if /I not "%~1"=="--elevated" (
+    echo Requesting administrator access...
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~f0' -ArgumentList '--elevated' -Verb RunAs"
+    exit /b
+)
+
 powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\windows\Install-PDPOne.ps1"
 pause
