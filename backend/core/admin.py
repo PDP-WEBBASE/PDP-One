8cf9973dd6bcf3beaa64ff1213354e2619c0e01e
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-07-18 09:25:58 +0330

    اتصال مالی و مطالبات به PostgreSQL و ChatGPT

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AnalysisReport, AuditEvent, Contract
+from .models import AnalysisReport, AuditEvent, Contract, PaymentReceipt, Receivable
 
 admin.site.register(Contract)
 admin.site.register(AnalysisReport)
 admin.site.register(AuditEvent)
-
+admin.site.register(Receivable)
+admin.site.register(PaymentReceipt)
