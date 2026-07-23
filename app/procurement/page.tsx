b4c2d278938790fb709b33270b07488626ed6ccd
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-07-23 11:49:55 +0330

    feat: show independent source toggles in procurement

--- app/procurement/page.tsx
+++ app/procurement/page.tsx
@@ -1,10 +1,14 @@
 import ConnectorHealthBanner from "./ConnectorHealthBanner";
+import ExtractionSourceControls from "./ExtractionSourceControls";
 import ProcurementWorkspaceV8 from "./ProcurementWorkspaceV8";
 
 export default function ProcurementPage() {
   return (
     <>
       <ConnectorHealthBanner />
+      <section style={{ maxWidth: 1440, margin: "16px auto 0", padding: "0 24px" }}>
+        <ExtractionSourceControls />
+      </section>
       <ProcurementWorkspaceV8 />
     </>
   );
