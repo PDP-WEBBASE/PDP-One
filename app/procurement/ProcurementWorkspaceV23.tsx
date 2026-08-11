427fa5228d34203ae81d6a1625556ddb75e490a6
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-08-11 22:43:57 +0330

    Activate organized procurement UI enhancements

---
+++ app/procurement/ProcurementWorkspaceV23.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ProcurementWorkspaceEnhancements from "./ProcurementWorkspaceEnhancements";
+import ProcurementWorkspaceV22 from "./ProcurementWorkspaceV22";
+
+export default function ProcurementWorkspaceV23() {
+  return <>
+    <ProcurementWorkspaceV22 />
+    <ProcurementWorkspaceEnhancements />
+  </>;
+}
