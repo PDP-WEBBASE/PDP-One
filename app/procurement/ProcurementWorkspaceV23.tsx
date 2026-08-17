ef9691efdb03c31594ceb228691c2039935512c9
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-08-17 22:43:40 +0330

    Activate pagination integrity safeguards

--- app/procurement/ProcurementWorkspaceV23.tsx
+++ app/procurement/ProcurementWorkspaceV23.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ProcurementPaginationEnhancement from "./ProcurementPaginationEnhancement";
+import ProcurementPaginationIntegrityEnhancement from "./ProcurementPaginationIntegrityEnhancement";
 import ProcurementSubmissionResultsEnhancements from "./ProcurementSubmissionResultsEnhancements";
 import ProcurementWorkspaceEnhancements from "./ProcurementWorkspaceEnhancements";
 import ProcurementWorkspaceV22 from "./ProcurementWorkspaceV22";
@@ -8,6 +9,7 @@ import ProcurementWorkspaceV22 from "./ProcurementWorkspaceV22";
 export default function ProcurementWorkspaceV23() {
   return <>
     <ProcurementPaginationEnhancement />
+    <ProcurementPaginationIntegrityEnhancement />
     <ProcurementWorkspaceV22 />
     <ProcurementWorkspaceEnhancements />
     <ProcurementSubmissionResultsEnhancements />
