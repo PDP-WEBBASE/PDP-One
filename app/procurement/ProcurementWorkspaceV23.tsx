8ca19acbb8a6431e1b0779dcb2fc4d29f62ba5a5
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-08-19 15:27:11 +0330

    Mount procurement tab cache layer

--- app/procurement/ProcurementWorkspaceV23.tsx
+++ app/procurement/ProcurementWorkspaceV23.tsx
@@ -2,6 +2,7 @@
 
 import ProcurementStartupSessionResilience from "./ProcurementStartupSessionResilience";
 import ProcurementPaginationEnhancement from "./ProcurementPaginationEnhancement";
+import ProcurementTabCacheEnhancement from "./ProcurementTabCacheEnhancement";
 import ProcurementPaginationIntegrityEnhancement from "./ProcurementPaginationIntegrityEnhancement";
 import ProcurementSubmissionResultsEnhancements from "./ProcurementSubmissionResultsEnhancements";
 import ProcurementWorkspaceEnhancements from "./ProcurementWorkspaceEnhancements";
@@ -11,6 +12,7 @@ export default function ProcurementWorkspaceV23() {
   return <>
     <ProcurementStartupSessionResilience />
     <ProcurementPaginationEnhancement />
+    <ProcurementTabCacheEnhancement />
     <ProcurementPaginationIntegrityEnhancement />
     <ProcurementWorkspaceV22 />
     <ProcurementWorkspaceEnhancements />
