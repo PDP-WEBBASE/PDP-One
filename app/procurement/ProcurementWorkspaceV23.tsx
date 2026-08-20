577b89c0da15e7513b777f435657ff7e560701ce
PDP-WEBBASE <maleki.pdp@gmail.com>
2026-08-20 14:31:52 +0330

    Wire compact procurement workflow enhancement

--- app/procurement/ProcurementWorkspaceV23.tsx
+++ app/procurement/ProcurementWorkspaceV23.tsx
@@ -7,6 +7,7 @@ import ProcurementManagementPerformanceEnhancement from "./ProcurementManagement
 import ProcurementNavigationReadCache from "./ProcurementNavigationReadCache";
 import ProcurementAnalysisContextInlineEnhancement from "./ProcurementAnalysisContextInlineEnhancement";
 import ProcurementPaginationIntegrityEnhancement from "./ProcurementPaginationIntegrityEnhancement";
+import ProcurementCompactWorkflowEnhancement from "./ProcurementCompactWorkflowEnhancement";
 import ProcurementSubmissionResultsEnhancements from "./ProcurementSubmissionResultsEnhancements";
 import ProcurementWorkspaceEnhancements from "./ProcurementWorkspaceEnhancements";
 import ProcurementWorkspaceV22 from "./ProcurementWorkspaceV22";
@@ -20,6 +21,7 @@ export default function ProcurementWorkspaceV23() {
     <ProcurementNavigationReadCache />
     <ProcurementAnalysisContextInlineEnhancement />
     <ProcurementPaginationIntegrityEnhancement />
+    <ProcurementCompactWorkflowEnhancement />
     <ProcurementWorkspaceV22 />
     <ProcurementWorkspaceEnhancements />
     <ProcurementSubmissionResultsEnhancements />
